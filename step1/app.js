3cd6e715401e8e3c58e0a5760480541541614150
mmeetty <mmeetty@gmail.com>
2016-05-04 11:01:22 -0500

    Update from the last class

--- step1/app.js
+++ step1/app.js
@@ -1,22 +1,37 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-//var sql = require("mssql");
-//var connectionString = process.env.MS_TableConnectionString; //my db credentials
 
 var app = express();
 
-// var filePath = "./datasets/*";
+var sql = require('mssql');
 
-// var jdata = fs.readFileSync(filePath);
-// var data = JSON.parse(jdata);
-// get id for a single episode
-//console.log(data._embedded.episodes[0].id);
+var recordset;
+
+var connectString = "Server=tcp:team1parkland.database.windows.net,1433;Database=team1parkland;Uid=cfleming12@team1parkland;Pwd=Sgtcf11c!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=3000;"
+
+
+
+app.get("/", function(req, res) {
+    sql.connect(connectString).then(function() {
+        new sql.Request().query('select top 25 * from NutritionData').then(function(recordset) {
+            res.render("index", {
+                Name: "Ingredient",
+                infos: recordset
+            });
+        });
+    }).catch(function(err) {
+    console.error(err);
+    // ... query error checks
+
+
+});
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
 // basic logging
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
     var log = "Request URL: " + req.url;
     console.log(log);
     next();
@@ -28,50 +43,52 @@ app.use(function(req, res, next){
 app.get("/api/search", require("./routes/api"));
 app.get("/api/list", require("./routes/api"));
 
-// app.get("/api/search", require("./routes/api"));
+// webpage part
+// var IngredientWrapper = function (recordset){app.get("/", function(req, res) {
+//     res.render("index", {
+//         Name: "Ingredient",
+//         infos: recordset
+//     });
+// });
+// };
 
+// app.get("/", function(req, res) {
+//     res.render("index", {
+//         Name: "Ingredient",
+//         infos: recordset
+//     });
+// });
 
-app.get("/", function(req, res){
-       res.render("index",{
-           Name: "Ingredient",
-           infos: ["one", "two", "three"]
-       }); 
-});
-// example!!!!!!!!!!
-// res.render('list', {
-//    rows: nutritionrecords
-//});
-
-app.get("/calculator", function(req, res){
-       res.render("calculator") 
-});
-
-app.get("/nutrition", function(req, res){
-       res.render("index", { 
-       Name: "Ingredient"
-    }); 
+app.get("/calculator", function(req, res) {
+    res.render("calculator")
 });
 
-app.get("/nutrition/:id", function(req, res){
-   var id = req.params.id;
-   var page = req.query.page; 
-   res.send("something here"); 
-   res.render("detail.html")
+app.get("/nutrition", function(req, res) {
+    res.render("index", {
+        Name: "Ingredient"
+    });
+});
+
+app.get("/nutrition/:id", function(req, res) {
+    var id = req.params.id;
+    var page = req.query.page;
+    res.send("something here");
+    res.render("detail.html")
 });
 
 // handle 404 errors for URLs that dont exist
-app.use(function(req, res, next){
-   var err = new Error("Page Not Found!");
-   err.status = 404;
-   next(err); 
+app.use(function(req, res, next) {
+    var err = new Error("Page Not Found!");
+    err.status = 404;
+    next(err);
 });
 
 // handle any and all errors
-app.use(function(err, req, res, next){
-   res.status(err.status || 500);
-   res.send(err.message);
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send(err.message);
 });
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function() {
     console.log('Example app listening on port 3000');
 });
--- step1/routes/api.js
+++ step1/routes/api.js
@@ -6,7 +6,26 @@ router.get("/search", function(req, res){
 });
 
 router.get("/list", function(req, res){
-    res.send("Hello World");
+    res.json()
 });
+
+//     var skip = (page - 1) * 25;
+
+//     var sql = `
+// SELECT    *
+// FROM      NutritionData
+// WHERE     Shrt_Desc LIKE '${searchText}'
+// LIMIT     25 OFFSET ${skip}
+// `;
+
+// // do the query
+// db.all(sql, function(err, records){
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+//     res.json(records);
+// })
+
 
 module.exports = router;
